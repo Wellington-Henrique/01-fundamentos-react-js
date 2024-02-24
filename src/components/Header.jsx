9164d31b7f308d4b844bac45a069253758f3e565
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-24 12:48:07 -0300

    Estrutura da Aplicação - Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import React from 'react'
+import IgniteLogo from '../assets/ignite-logo.svg'
+
 import styles from './Header.module.css'
 
 export function Header() {
     return(
         <header className={styles.header}>
-            <strong>Ignite Feed</strong>
+            <img src={IgniteLogo} alt="" />
         </header>
     )
 }
