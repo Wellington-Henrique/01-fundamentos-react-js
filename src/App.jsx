b33eed55c407722a1e1d9938d60aba63e09295a9
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-24 21:10:40 -0300

    Estrutura da Aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from './App.module.css'
 
 function App() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main></main>
+      </div>
+    </div>
   )
 }
 
