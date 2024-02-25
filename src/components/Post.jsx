9a0f4393bf3442f5586d29d0150b573a6b5c6637
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-24 22:05:37 -0300

    Estrutura da Aplicação - Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -26,9 +26,13 @@ export function Post() {
 
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 <a href="#">jane.design/doctorcare</a></p>
-                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz na especialização de em React da Rocketseat. 🚀</p>
+                <p>👉<a href="#">https://github.com/Wellington-Henrique</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>
+                    <a href="#">#nlw</a>
+                    <a href="#">#rocketseat</a>
+                </p>
             </div>
         </article>
     )
