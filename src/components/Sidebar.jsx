0c1091b79b376d949f2876062c7e01d368609fe3
Wellington Henrique <wellingtonhlc@gmail.com>
2024-02-24 21:30:31 -0300

    Estrutura da Aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from  './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return(
@@ -9,12 +10,20 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar} 
+                    src="https://github.com/Wellington-Henrique.png"
+                />
+                
                 <strong>Wellington Henrique</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
